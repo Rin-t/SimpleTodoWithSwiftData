7a309e00aa1377ce7b0fc3141268e80312f90cd7
Rin-t <rin.t.4921@gmail.com>
2023-11-14 22:03:44 +0900

    Initial Commit

---
+++ SimpleTodoWithSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleTodoWithSwiftData
+//
+//  Created by Rin on 2023/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleTodoWithSwiftData/SimpleTodoWithSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleTodoWithSwiftDataApp.swift
+//  SimpleTodoWithSwiftData
+//
+//  Created by Rin on 2023/11/14.
+//
+
+import SwiftUI
+
+@main
+struct SimpleTodoWithSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
