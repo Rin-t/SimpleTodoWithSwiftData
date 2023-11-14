fc1473b772895bf662b0e67d509198302fa301e5
Rin-t <rin.t.4921@gmail.com>
2023-11-14 22:19:37 +0900

    ToDoModelの追加

---
+++ SimpleTodoWithSwiftData/ToDo.swift
@@ -0,0 +1,22 @@
+//
+//  ToDo.swift
+//  SimpleTodoWithSwiftData
+//
+//  Created by Rin on 2023/11/14.
+//
+
+import Foundation
+import SwiftData
+
+
+@Model
+final class ToDo {
+    let task: String
+    let createdAt: Date
+    var isCompleted = false
+
+    init(task: String, createdAt: Date) {
+        self.task = task
+        self.createdAt = createdAt
+    }
+}
