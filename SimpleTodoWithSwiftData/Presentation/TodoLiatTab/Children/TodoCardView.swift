2542e332a66ff813c623961c211b33c3a5fbe8f5
Rin-t <rin.t.4921@gmail.com>
2023-11-22 22:15:59 +0900

    add: todoのセルのViewを作成

---
+++ SimpleTodoWithSwiftData/Presentation/TodoLiatTab/Children/TodoCardView.swift
@@ -0,0 +1,49 @@
+//
+//  TodoCardView.swift
+//  SimpleTodoWithSwiftData
+//
+//  Created by Rin on 2023/11/18.
+//
+
+import SwiftUI
+
+struct TodoCardView: View {
+
+    private let todo: Todo
+
+    init(todo: Todo) {
+        self.todo = todo
+    }
+
+    var body: some View {
+        HStack {
+
+            Image(systemName: todo.isCompleted ? 
+                  "checkmark.square" : "square")
+            .resizable()
+            .frame(width: 20, height: 20)
+            .padding(.trailing, 8)
+
+            VStack(alignment: .leading) {
+                Text("\(todo.task)")
+                    .padding(.bottom, 8)
+                    .font(.title)
+                Text("\(todo.category?.name ?? "カテゴリなし")")
+                    .foregroundStyle(.gray)
+            }
+
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                Spacer()
+                Text("作成日")
+                Text("\(todo.createdAt.string())")
+            }
+        }
+    }
+}
+
+#Preview {
+    let todo = Todo(task: "TDDの勉強")
+    return TodoCardView(todo: todo)
+}
